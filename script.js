92d3cbdda3efcc0e4766b8f22491836d5d2f5513
Benedict okoro <benedictokoro84@gmail.com>
2025-12-31 14:38:48 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const faqItems = document.querySelectorAll('.enquiry');
+
+faqItems.forEach(item => {
+  item.querySelector('.enquiry_btn').addEventListener('click', () =>{
+
+    const alreadyOpen = item.classList.contains('open')
+
+    faqItems.forEach(i=> i.classList.remove('open') )
+
+    if(!alreadyOpen) {
+    item.classList.add('open')
+   }
+  })
+})
